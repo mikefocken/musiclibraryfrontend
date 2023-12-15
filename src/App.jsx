a983ad8f9afa71acb9332c064cc38c55b94f48b5
mike focken <mikefocken@gmail.com>
2023-12-15 14:11:37 -0600

    Music Table component started

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import axios from "axios";
 import React from "react";
 import Header from "./Components/Header/Header";
+import MusicTable from "./Components/MusicTable/MusicTable";
 
 function App() {
   const fetchSongs = async () => {
@@ -11,6 +12,7 @@ function App() {
   return (
     <div className="App">
       <Header />
+      <MusicTable/>
       <button onClick={fetchSongs}>fetchSongs</button>
     </div>
   );
---
+++ src/Components/MusicTable/MusicTable.jsx
@@ -0,0 +1,13 @@
+const MusicTable = ({}) => {
+  return (
+    <div className="FlexTitles">
+      <h4>Title</h4>
+      <h4>Album</h4>
+      <h4>Artist</h4>
+      <h4>Genre</h4>
+      <h4>Release Date</h4>
+    </div>
+  );
+};
+
+export default MusicTable;
