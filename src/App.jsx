9bae58493269f64b71f57557115b2b77b172a939
mike focken <mikefocken@gmail.com>
2023-12-15 13:11:42 -0600

    added heaeder compenent

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
 import "./App.css";
+import axios from "axios";
+import React from "react";
 
 function App() {
-  return <div className="App"></div>;
+  const fetchSongs = async () => {
+    const response = await axios.get("http://localhost:5176/api/Songs");
+    console.log(response);
+  };
+  return (
+    <div className="App">
+      <button onClick={fetchSongs}>fetchSongs</button>
+    </div>
+  );
 }
 
 export default App;
